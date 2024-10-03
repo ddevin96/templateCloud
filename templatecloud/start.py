8e5d701cfaa74f7034919d8441a796e46c78ecb6
ddevin96 <ddevinco@unisa.it>
2024-10-03 13:42:00 +0000

    import libraries

--- templatecloud/start.py
+++ templatecloud/start.py
@@ -1,6 +1,9 @@
 import os
 from time import time
+import numpy as np 
 
 if __name__ == "__main__":
     print("hello from plugin")
     print(time())
+    c = np.zeros([3, 3])
+    print("\nMatrix c : \n", c)
