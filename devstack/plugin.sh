5eae4b04485a11b21a602d28122ffc9e3c5c54a8
ddevin96 <ddevinco@unisa.it>
2024-10-03 10:05:34 +0000

    ahjsf

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -18,22 +18,22 @@ if is_service_enabled templatecloud; then
 
     if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
         # Set up system services
-        echo_summary "Configuring system services Template"
+        echo_summary "Configuring templatecloud"
         install_package cowsay
 
     elif [[ "$1" == "stack" && "$2" == "install" ]]; then
         # Perform installation of service source
-        echo_summary "Installing Template"
+        echo_summary "Installing templatecloud"
         install_template
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         # Configure after the other layer 1 and 2 services have been configured
-        echo_summary "Configuring Template"
+        echo_summary "Configuring templatecloud"
         configure_template
 
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         # Initialize and start the template service
-        echo_summary "Initializing Template"
+        echo_summary "Initializing templatecloud"
         init_template
     fi
 
