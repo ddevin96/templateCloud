51b5cfcecc73516ee48fd5056f373bbf7d18021d
ddevin96 <ddevinco@unisa.it>
2024-10-24 11:03:43 +0000

    flask

--- templatecloud/start.py
+++ templatecloud/start.py
@@ -1,4 +1,5 @@
 import os
+import threading
 from time import time
 from flask import Flask
 
@@ -6,5 +7,10 @@ from flask import Flask
 def hello_world():
     return 'Hello, World!'    
 
+def run_flask():
+    app.run(host="0.0.0.0", port=5234)
+
 if __name__ == "__main__":
     app = Flask(__name__)
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
