e647a610e4dddfa9dad640414b77081170bd356e
ddevin96 <ddevinco@unisa.it>
2024-10-24 10:57:32 +0000

    add

--- templatecloud/start.py
+++ templatecloud/start.py
@@ -1,19 +1,10 @@
 import os
 from time import time
-import numpy as np 
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'    
 
 if __name__ == "__main__":
-    print("hello from plugin")
-    print(time())
-    c = np.zeros([3, 3])
-    print("\nMatrix c : \n", c)
-    response = requests.get("https://www.google.com")
-    if response.status_code != 200:
-        print(f"Error: {response.status_code}")
-    else:
-        print(f"response: {response.status_code}")
-
-    soup = BeautifulSoup(response.content, "html.parser")
-    print(soup)
+    app = Flask(__name__)
